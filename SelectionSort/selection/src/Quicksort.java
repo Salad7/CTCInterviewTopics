27aa7e0e3f03854ac405a8d80749f88bfca221ca
mohamedsalad <kukgiplayer2@gmail.com>
2022-10-12 13:00:40 -0400

    quickie sort

--- SelectionSort/selection/src/Quicksort.java
+++ SelectionSort/selection/src/Quicksort.java
@@ -4,18 +4,25 @@ public class Quicksort {
     int openSpace = 0;
     public Quicksort(){
         runAlgorithm(0,nums.length-1, nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-        }
+//        runAlgorithm(0,7, nums);
+
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+//        }
 
     }
 
     public void runAlgorithm(int startIndex, int endIndex, int[] nums) {
+        int endingIndex = endIndex;
+        int startingIndex = startIndex;
         if (startIndex == endIndex) {
             //Done
-            numNums[openSpace] = nums[startIndex];
-            openSpace = openSpace + 1;
+            System.out.println("Placing "+nums[startIndex]+" at open space: "+openSpace);
+//            numNums[openSpace] = nums[startIndex];
+//            openSpace = openSpace + 1;
+            System.out.println("Open space is now: "+openSpace);
         } else {
+
             int midPoint = (startIndex + endIndex) / 2;
             System.out.println("Midpoint Index: " + midPoint + " value: " + nums[midPoint]);
             while (startIndex != endIndex - 1) {
@@ -37,9 +44,14 @@ public class Quicksort {
                         endIndex = endIndex - 1;
                         if (endIndex - 1 == startIndex) {
                             //The next index is at the pivot, so if the right value is less then startIndex, do one swap and move on
-                            int tempVal = leftVal;
-                            nums[startIndex] = rightVal;
-                            nums[endIndex] = tempVal;
+                            int tempVal = nums[midPoint];
+                            System.out.println("The pivots are next to each other: comparing: ");
+                            if(tempVal < nums[startIndex]) {
+                                System.out.println(tempVal + " is less then: " + nums[startIndex]);
+                                System.out.println("Swapping " + tempVal+ " and " + nums[startIndex]);
+                                nums[midPoint] = nums[startIndex];
+                                nums[startIndex] = tempVal;
+                            }
                         }
                     }
                 } else {
@@ -53,8 +65,11 @@ public class Quicksort {
             for (int i = 0; i < nums.length; i++) {
                 System.out.println(nums[i]);
             }
+            System.out.println("Running algorithm on Index: "+0+" to Index: "+startIndex);
+            System.out.println("Running algorithm on Index: "+(endIndex)+" to Index: "+endingIndex);
+
             runAlgorithm(0,startIndex,nums);
-            runAlgorithm(startIndex,nums.length-1,nums );
+            runAlgorithm(endIndex, endingIndex, nums );
         }
     }
 }
